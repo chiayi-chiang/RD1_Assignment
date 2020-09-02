e8c3d236ee3d24d6473d8ce92489369f0e1a1de0
CS01419 <65748093+Kongenrnd@users.noreply.github.com>
2020-09-02 18:20:45 +0800

    RD1

---
+++ XML_JSON/XML/Demo3_CreateNewXML.php
@@ -0,0 +1,9 @@
+﻿<?php
+$doc = new DOMDocument("1.0", "utf-8");
+$root = $doc->createElement("HTML");
+$root2 = $doc->createElement("CSS");
+//<HTML><CSS></CSS> </HTML>
+$doc->appendChild($root);//找DOMDocument
+$root->appendChild($root2);
+echo htmlspecialchars($doc->c14n());
+?>
---
+++ text.php
@@ -0,0 +1,14 @@
+<?php
+    private $_strInsertManyBase = "INSERT INTO `orderdetails`(`orderID`, `commodityID`, `orderCommodityPrice`, `orderCommodityQuantity`) VALUES";for ($i = 0; $i < count($orderDetails); $i++) {
+    $this->_strInsertManyBase .= "(:orderID{$i},:commodityID{$i},:orderCommodityPrice{$i},:orderCommodityQuantity{$i})";
+    if ($i < (count($orderDetails) - 1)) {
+    $this->_strInsertManyBase .= ",";
+    }
+    }foreach ($orderDetails as $key => $item) {
+    $sth->bindParam("orderID$key", $orderID);
+    $sth->bindParam("commodityID$key", $item->getCommodityID());
+    $sth->bindParam("orderCommodityPrice$key", $item->getOrderCommodityPrice());
+    $sth->bindParam("orderCommodityQuantity$key", $item->getOrderCommodityQuantity());
+    }
+    $sth
+?>
